4e1b1e5b32e365729b5571e92c2e033099c24f70
MaicoL Segura <maicol3098@hotmail.com>
2024-07-26 19:04:10 -0600

    Se creo form de nueva solicitud

--- NuevaSolicitud.vb
+++ NuevaSolicitud.vb
@@ -29,37 +29,7 @@
     Private Sub NuevaSolicitud_Load(sender As Object, e As EventArgs) Handles MyBase.Load
     End Sub
 
-    Private Sub btSInsertar_Click(sender As Object, e As EventArgs) Handles btSInsertar.Click
-        ' Obtener valores del form
-        Dim fechaInicio As Date = DateTimePickerFechaInicio.Value
-        Dim fechaFin As Date = DateTimePickerFechaFinal.Value
-        Dim horas As Integer = 8 * (fechaFin - fechaInicio).Days
 
-        ' verificar si el usuario existe en la base de datos
-        Dim consultaUsuario As String = $"SELECT * FROM USERS WHERE USUARIO = '{vusuario}'"
-        conexion.consultar(consultaUsuario, "USERS")
-
-        If conexion.ds.Tables("USERS").Rows.Count > 0 Then
-            ' usuario encontrado
-            Dim userRow As DataRow = conexion.ds.Tables("USERS").Rows(0)
-            Dim identificacion As String = userRow("IDENTIFICACION").ToString()
-            Dim nombre As String = userRow("NOMBRE").ToString()
-            Dim apellido As String = userRow("APELLIDO").ToString()
-
-            ' insertar el usuario a la base de datos
-            Dim consultaInsertar As String = $"INSERT INTO SOLICITUDES (IDENTIFICATION, NOMBRE, APELLIDO, HORAS, FECHA_FIN, FECHA_INICIO, ESTADO, EMAIL) " &
-                                             $"VALUES ('{identificacion}', '{nombre}', '{apellido}', {horas}, '{fechaFin:yyyy-MM-dd}', '{fechaInicio:yyyy-MM-dd}', 'Solicitado', '{vusuario}')"
-
-            If conexion.inserta_datos(consultaInsertar) Then
-                MessageBox.Show("Solicitud insertada correctamente")
-            Else
-                MessageBox.Show("Error al insertar la solicitud")
-            End If
-        Else
-            ' usuario no encontrado
-            MessageBox.Show("Usuario no encontrado")
-        End If
-    End Sub
 
     Private Sub DateTimePickerFechaInicio_ValueChanged(sender As Object, e As EventArgs) Handles DateTimePickerFechaInicio.ValueChanged
     End Sub
